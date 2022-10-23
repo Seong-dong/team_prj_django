808b8256314c9599389c5e1ebc4ad97bd53c0c5e
Seong-dong <darkquiz@naver.com>
2022-10-23 23:13:49 +0900

    첨부파일 있을경우 포스트 상세페이지에 다운로드 버튼 출력

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-# Create your models here.
+import os
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
@@ -16,5 +15,14 @@ class Post(models.Model):
     def __str__(self):
         return f'[{self.pk}] {self.title}' #pk는 장고에서 제공하는 id값
 
+
     def get_absolute_url(self): #get_absolute_url은 장고에서 젝오하는 기능.
         return f'/blog/{self.pk}/'
+
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
