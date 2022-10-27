f3f5a44a5fe2b7acdfb19bf4813bf9b30704a098
Seong-dong <darkquiz@naver.com>
2022-10-28 00:10:01 +0900

    포스트 목록 및 디테일 카테고리 기능 추가

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
 
 
 class PostList(ListView):
     ##post_list.html로 만들어야함
-    model = Post
+    model = Post # 기본설정post_list쪽으로 넘겨줌.
     # template_name = 'blog/index.html' #지정페이지사용법
     ordering = '-pk'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
 
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
@@ -22,7 +28,7 @@ class PostList(ListView):
 
 class PostDetail(DetailView):
     ##post_list.html로 만들어야함
-    model = Post
+    model = Post # 기본설정post_detail쪽으로 넘겨줌.
 # def single_post_page(request, pk):
 #     post = Post.objects.get(pk=pk)
 #
