9837302352b844b21d412bfa7e31a164b534c30f
Seong-dong <darkquiz@naver.com>
2022-11-09 17:28:31 +0900

    pagination 추가

--- blog/views.py
+++ blog/views.py
@@ -13,6 +13,7 @@ class PostList(ListView):
     model = Post # 기본설정post_list쪽으로 넘겨줌.
     # template_name = 'blog/index.html' #지정페이지사용법
     ordering = '-pk'
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
