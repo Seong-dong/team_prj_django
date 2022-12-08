4cccd38405a4f0aae743081effedede17f443d86
Seong-dong <darkquiz@naver.com>
2022-12-09 00:24:25 +0900

    게시판 기능 추가

--- board/models.py
+++ board/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Board(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title} :: {self.author}'
+
+    def get_absolute_url(self): #get_absolute_url은 장고에서 젝오하는 기능.
+        return f'/board/{self.pk}/'
--- board/views.py
+++ board/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Board
 # Create your views here.
+
+
+
+class BoardList(ListView):
+    ##board_list.html로 만들어야함
+    model = Board
+    # template_name = 'blog/index.html' #지정페이지사용법
+    ordering = '-pk'
+    # paginate_by = 5
+    #
+    # def get_context_data(self, **kwargs):
+    #     context = super(PostList, self).get_context_data()
+    #     context['categories'] = Category.objects.all()
+    #     context['no_category_post_count'] = Post.objects.filter(category=None).count()
+    #     return context
+
+class BoardDetail(DetailView):
+    model = Board
+
+class BoardCreate(LoginRequiredMixin, CreateView):
+    model = Board
+    fields = ['title', 'content']
+    #
+    # def test_func(self):
+    #     return self.request.user.is_superuser or self.request.user.is_staff or self.request.user.is_active #유저 권한 체크
+
+
+    def form_valid(self, form): #form 은 현재 class의 instance
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            response = super(BoardCreate, self).form_valid(form)
+
+            return response
+        else:
+            return redirect('/board/')
