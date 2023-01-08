df60d7b1338eefd4154e939911cfb4f6c0f5c956
Seong-dong <darkquiz@naver.com>
2023-01-08 13:03:01 +0900

    서머노트 출력 수정

--- django_blog_sdjo/settings.py
+++ django_blog_sdjo/settings.py
@@ -23,9 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-20jragcfau)_q$yz98z5op_5q6x*vpv7562d1v#6g*%u#&#brl')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = int(os.environ.get('DEBUG', 1))
-# DEBUG = True
-DEBUG = False
+DEBUG = int(os.environ.get('DEBUG', 1))
 
 # if os.environ.get('DJANGO_ALLOWED_HOSTS'):
 #     ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
@@ -146,8 +144,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '_static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '_static') #저장경로
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media') #저장경로
